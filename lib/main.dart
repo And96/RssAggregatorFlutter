1adad28f3eea93ff8e634c8f1256818ff1777af3
aa <42185838+And96@users.noreply.github.com>
2022-09-05 22:36:35 +0200

    fix to padding

--- lib/main.dart
+++ lib/main.dart
@@ -113,69 +113,76 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Stack(
         children: [
           isLoading == false
-              ? Scrollbar(
-                  child: ListView.separated(
-                      itemCount: rss.items!.length,
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                      itemBuilder: (BuildContext context, index) {
-                        final item = rss.items![index];
-
-                        return InkWell(
-                          onTap: () async {
-                            _launchInBrowser(Uri.parse((item.link.toString())));
+              ? Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Scrollbar(
+                      child: ListView.separated(
+                          itemCount: rss.items!.length,
+                          separatorBuilder: (context, index) {
+                            return const Divider();
                           },
-                          child: ListTile(
-                              leading: const Icon(Icons.rss_feed),
-                              title: Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  (Uri.parse(item.link.toString())
-                                      .host
-                                      .toString()),
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.normal,
-                                    color: Color.fromARGB(255, 90, 90, 90),
+                          itemBuilder: (BuildContext context, index) {
+                            final item = rss.items![index];
+
+                            return InkWell(
+                              onTap: () async {
+                                _launchInBrowser(
+                                    Uri.parse((item.link.toString())));
+                              },
+                              child: ListTile(
+                                  leading: const Icon(Icons.rss_feed),
+                                  title: Padding(
+                                    padding: const EdgeInsets.only(top: 0),
+                                    child: Text(
+                                      (Uri.parse(item.link.toString())
+                                          .host
+                                          .toString()),
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.normal,
+                                        color: Color.fromARGB(255, 90, 90, 90),
+                                      ),
+                                    ),
                                   ),
-                                ),
-                              ),
-                              isThreeLine: true,
-                              subtitle: Padding(
-                                  padding: const EdgeInsets.only(top: 5),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      SizedBox(
-                                        child: Text(
-                                          item.title.toString(),
-                                          maxLines: 2,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.normal,
-                                            color:
-                                                Color.fromARGB(255, 20, 20, 20),
+                                  isThreeLine: true,
+                                  subtitle: Padding(
+                                      padding: const EdgeInsets.only(top: 5),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          SizedBox(
+                                            child: Text(
+                                              item.title.toString(),
+                                              maxLines: 2,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.normal,
+                                                color: Color.fromARGB(
+                                                    255, 20, 20, 20),
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(top: 5),
-                                        child: Row(
-                                          children: [
-                                            Text(DateFormat('dd/MM/yyyy hh:mm')
-                                                .format(DateTime.parse(
-                                                    item.pubDate.toString()))),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ))),
-                        );
-                      }))
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 5),
+                                            child: Row(
+                                              children: [
+                                                Text(DateFormat(
+                                                        'dd/MM/yyyy hh:mm')
+                                                    .format(DateTime.parse(item
+                                                        .pubDate
+                                                        .toString()))),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ))),
+                            );
+                          })),
+                )
               : const Center(
                   child: CircularProgressIndicator(),
                 ),
