ed5d7480d0e0bb615922945d44ca93f3a7076fa9
aa <42185838+And96@users.noreply.github.com>
2022-10-24 23:00:12 +0200

    Theming category reccomendations

---
+++ lib/core/recommended_list.dart
@@ -0,0 +1,190 @@
+import 'dart:convert';
+//import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class RecommendedCategory {
+  RecommendedCategory(
+      this.name, this.color, this.iconData, this.language, this.sites);
+  final String name;
+  final int color;
+  final int iconData;
+  final String language;
+  final List<RecommendedSite> sites;
+
+  factory RecommendedCategory.fromJson(Map<String, dynamic> data) {
+    final name = data['name'] as String;
+    final color = data['color'] as int;
+    final iconData = data['iconData'] as int;
+    final language = data['language'] as String;
+    final sitesData = data['sites'] as List<dynamic>?;
+    final sites = sitesData != null
+        ? sitesData
+            .map((reviewData) => RecommendedSite.fromJson(reviewData))
+            .toList()
+        : <RecommendedSite>[];
+    return RecommendedCategory(name, color, iconData, language, sites);
+  }
+}
+
+class RecommendedSite {
+  RecommendedSite(this.siteLink, this.iconUrl, this.category, this.siteName);
+  final String siteName;
+  final String siteLink;
+  final String iconUrl;
+  final String category;
+
+  factory RecommendedSite.fromJson(Map<String, dynamic> data) {
+    final siteName = data['siteName'] as String;
+    final siteLink = data['siteLink'] as String;
+    final iconUrl = data['iconUrl'] as String;
+    final category = data['category'] as String;
+    return RecommendedSite(siteName, siteLink, iconUrl, category);
+  }
+}
+
+class RecommendedList {
+  late List<RecommendedCategory> items = [];
+
+//0xe50c
+
+  String json = """[{
+		"name": "Tecnologia",
+		"color": 4283215696,
+		"iconData": 58865,
+		"language": "it",
+		"sites": [{
+				"siteName": "tuttoandroid.net",
+				"siteLink": "https://tuttoandroid.net/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/tuttoandroid.net.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "hdblog.it",
+				"siteLink": "https://hdblog.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/hdblog.it.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "androidworld.it",
+				"siteLink": "https://androidworld.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/androidworld.it.ico",
+				"category": "News"
+			}
+		]
+	},
+	{
+		"name": "Sport",
+		"color": 4278351805,
+		"iconData": 58302,
+		"language": "it",
+		"sites": [{
+				"siteName": "gazzetta.it",
+				"siteLink": "https://tuttoandroid.net/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/tuttoandroid.net.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "hdblog.it",
+				"siteLink": "https://hdblog.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/hdblog.it.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "androidworld.it",
+				"siteLink": "https://androidworld.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/androidworld.it.ico",
+				"category": "News"
+			}
+		]
+	}
+  ,
+	{
+		"name": "Bergamo",
+		"color": 4289533015,
+		"iconData": 58866,
+		"language": "it",
+		"sites": [{
+				"siteName": "gazzetta.it",
+				"siteLink": "https://tuttoandroid.net/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/tuttoandroid.net.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "hdblog.it",
+				"siteLink": "https://hdblog.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/hdblog.it.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "androidworld.it",
+				"siteLink": "https://androidworld.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/androidworld.it.ico",
+				"category": "News"
+			}
+		]
+	}
+  ,
+	{
+		"name": "Gossip",
+		"color": 4279060385,
+		"iconData": 61871,
+		"language": "it",
+		"sites": [{
+				"siteName": "gazzetta.it",
+				"siteLink": "https://tuttoandroid.net/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/tuttoandroid.net.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "hdblog.it",
+				"siteLink": "https://hdblog.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/hdblog.it.ico",
+				"category": "News"
+			},
+			{
+				"siteName": "androidworld.it",
+				"siteLink": "https://androidworld.it/feed/",
+				"iconUrl": "https://icons.duckduckgo.com/ip3/androidworld.it.ico",
+				"category": "News"
+			}
+		]
+	}
+]""";
+
+  Future<bool> load() async {
+    try {
+      await save(json);
+      items = await get();
+      return true;
+    } catch (err) {
+      // print('Caught error: $err');
+    }
+    return false;
+  }
+
+  Future<void> save(String jsonRecommended) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+
+      await prefs.setString('db_recommended', jsonRecommended);
+    } catch (err) {
+      //print('Caught error: $err');
+    }
+  }
+
+  Future<List<RecommendedCategory>> get() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final List<dynamic> jsonData =
+          await jsonDecode(prefs.getString('db_recommended') ?? '[]');
+      late List<RecommendedCategory> list = List<RecommendedCategory>.from(
+          jsonData.map((model) => RecommendedCategory.fromJson(model)));
+      //sort
+      //items.sort((a, b) => b.pubDate!.compareTo(a.pubDate!));
+      return list;
+    } catch (err) {
+      // print('Caught error: $err');
+    }
+    return [];
+  }
+}
--- lib/screens/recommended_category_page.dart
+++ lib/screens/recommended_category_page.dart
@@ -1,15 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:rss_aggregator_flutter/core/recommended_category_list.dart';
-import 'package:rss_aggregator_flutter/core/feed.dart';
-import 'package:rss_aggregator_flutter/core/utility.dart';
-import 'package:flutter/services.dart';
+import 'package:rss_aggregator_flutter/core/recommended_list.dart';
 import 'package:rss_aggregator_flutter/theme/theme_color.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:rss_aggregator_flutter/widgets/empty_section.dart';
-// ignore: depend_on_referenced_packages
-/*import 'dart:async';*/
 
 class RecommendedCategoryPage extends StatefulWidget {
   const RecommendedCategoryPage({Key? key}) : super(key: key);
@@ -19,12 +11,13 @@ class RecommendedCategoryPage extends StatefulWidget {
       _RecommendedCategoryPageState();
 }
 
+List<String> list = <String>['Italiano', 'English', 'Spanish', 'India'];
+
 class _RecommendedCategoryPageState extends State<RecommendedCategoryPage>
     with SingleTickerProviderStateMixin {
   bool isLoading = false;
   double progressLoading = 0;
-  late RecommendedCategoryList recommendedCategoryList =
-      RecommendedCategoryList();
+  late RecommendedList recommendedList = RecommendedList();
   bool darkMode = false;
 
   @override
@@ -49,120 +42,12 @@ class _RecommendedCategoryPageState extends State<RecommendedCategoryPage>
       AnimationController(vsync: this, duration: const Duration(seconds: 2))
         ..repeat();
 
-  void showOptionDialog(BuildContext context, Feed item) {
-    var dialog = SimpleDialog(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          const Text(
-            "Options",
-          ),
-          IconButton(
-            padding: EdgeInsets.zero,
-            constraints: const BoxConstraints(),
-            icon: const Icon(Icons.close),
-            tooltip: 'Close',
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-      contentPadding: const EdgeInsets.all(8),
-      children: <Widget>[
-        const Divider(),
-        ListTile(
-          leading: const Icon(Icons.open_in_new),
-          title: const Text('Open site'),
-          onTap: () async {
-            Utility().launchInBrowser(Uri.parse(item.link));
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.copy),
-          title: const Text('Copy link'),
-          onTap: () {
-            Clipboard.setData(ClipboardData(text: item.link));
-            Navigator.pop(context);
-            const snackBar = SnackBar(
-              duration: Duration(milliseconds: 500),
-              content: Text('Link copied to clipboard'),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.share),
-          title: const Text('Share link'),
-          onTap: () {
-            Share.share(item.link);
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: const Icon(Icons.delete),
-          title: const Text('Delete link'),
-          onTap: () {
-            setState(() {
-              recommendedCategoryList.delete(item.link);
-            });
-            Navigator.pop(context);
-            const snackBar = SnackBar(
-              duration: Duration(seconds: 1),
-              content: Text('Deleted'),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
-          //onTap: showDeleteAlertDialog(context, url),
-        ),
-      ],
-    );
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return dialog;
-        });
-  }
-
-  showDeleteDialog(BuildContext context, String url) {
-    // set up the buttons
-    Widget cancelButton = TextButton(
-      child: const Text("Yes"),
-      onPressed: () {
-        setState(() {
-          recommendedCategoryList.delete(url);
-        });
-        Navigator.pop(context);
-      },
-    );
-    Widget continueButton = TextButton(
-      child: const Text("No"),
-      onPressed: () {
-        Navigator.pop(context);
-      },
-    );
-    AlertDialog alert = AlertDialog(
-      content: const Text("Delete all items?"),
-      actions: [
-        cancelButton,
-        continueButton,
-      ],
-    );
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
-
   loadData() async {
     try {
       setState(() {
         isLoading = true;
       });
-      await recommendedCategoryList.load();
+      await recommendedList.load();
     } catch (err) {
       //print('Caught error: $err');
     }
@@ -171,105 +56,97 @@ class _RecommendedCategoryPageState extends State<RecommendedCategoryPage>
     });
   }
 
+  String dropdownValue = list.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: recommendedCategoryList.items.isEmpty
-              ? const Text('RecommendedCategory')
-              : Text(
-                  'RecommendedCategory (${recommendedCategoryList.items.length})'),
-          actions: <Widget>[
-            if (isLoading)
-              IconButton(
-                icon: AnimatedBuilder(
-                  animation: _refreshIconController,
-                  builder: (_, child) {
-                    return Transform.rotate(
-                      angle: _refreshIconController.value * 3 * 3.1415,
-                      child: child,
-                    );
+        appBar: AppBar(title: const Text('Recommendations'), actions: <Widget>[
+          isLoading
+              ? IconButton(
+                  icon: AnimatedBuilder(
+                    animation: _refreshIconController,
+                    builder: (_, child) {
+                      return Transform.rotate(
+                        angle: _refreshIconController.value * 3 * 3.1415,
+                        child: child,
+                      );
+                    },
+                    child: const Icon(Icons.autorenew),
+                  ),
+                  onPressed: () => {},
+                )
+              : DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                  value: dropdownValue,
+                  style: const TextStyle(color: Colors.white),
+                  dropdownColor: ThemeColor.primaryColorLight,
+                  //underline: SizedBox(),
+                  //iconEnabledColor: Colors.white,
+                  //focusColor: Colors.white,
+                  //iconDisabledColor: Colors.white,
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      dropdownValue = value!;
+                    });
                   },
-                  child: const Icon(Icons.autorenew),
-                ),
-                onPressed: () => {},
-              ),
-            if (recommendedCategoryList.items.isNotEmpty && !isLoading)
-              IconButton(
-                  icon: const Icon(Icons.delete),
-                  tooltip: 'Delete',
-                  onPressed: () => showDeleteDialog(context, "*")),
-          ],
-        ),
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ))
+        ]),
         body: Stack(
           children: [
             isLoading == false
-                ? GridView.count(
-                    // Create a grid with 2 columns. If you change the scrollDirection to
-                    // horizontal, this produces 2 rows.
-                    crossAxisCount: 2,
-
-                    // Generate 100 wiprimary: false,
-                    padding: const EdgeInsets.all(1.5),
-                    childAspectRatio: 0.80,
-                    mainAxisSpacing: 1.0,
-                    crossAxisSpacing: 1.0,
-                    shrinkWrap: true,
-                    children: List.generate(100, (index) {
-                      return Card(
-                        elevation: 2.0,
-                        color: Colors.primaries[
-                            Random().nextInt(Colors.primaries.length)],
-                        child: InkWell(
-                          //highlightColor: Colors.white.withAlpha(30),
-                          //splashColor: Colors.white.withAlpha(20),
-                          child: GridTile(
-                            // header section
-                            //header: const Icon(Icons.favorite),
-                            footer: GridTileBar(
-                              backgroundColor: Colors.white.withAlpha(40),
-                              title: Text(
-                                'Categoria $index',
-                                textAlign: TextAlign.center,
-                              ),
-
-                              /*trailing: const Icon(
-                                Icons.bookmark_outline,
-                                //color: Colors.black,
-                              ),*/
-                            ),
-                            child: const Icon(
-                              Icons.favorite,
-                              color: Colors.white,
-                              size: 75,
-                            ),
-                          ),
-
-                          /*Center(
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                mainAxisSize: MainAxisSize.min,
-                                verticalDirection: VerticalDirection.down,
-                                children: <Widget>[
-                                  const Icon(Icons.favorite),
-                                  Center(
-                                    child: Text('Item $index'),
+                ? GridView.builder(
+                    itemCount: recommendedList.items.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: MediaQuery.of(context).orientation ==
+                              Orientation.landscape
+                          ? 3
+                          : 2,
+                      crossAxisSpacing: 1,
+                      mainAxisSpacing: 1,
+                      childAspectRatio: MediaQuery.of(context).orientation ==
+                              Orientation.landscape
+                          ? 1.6
+                          : 0.80,
+                    ),
+                    itemBuilder: (
+                      context,
+                      index,
+                    ) {
+                      return GestureDetector(
+                          onTap: () {
+                            //Navigator.of(context).pushNamed(RouteName.GridViewCustom);
+                          },
+                          child: Card(
+                            elevation: 2.0,
+                            color: Color(recommendedList.items[index].color),
+                            child: InkWell(
+                              child: GridTile(
+                                footer: GridTileBar(
+                                  backgroundColor: Colors.white.withAlpha(40),
+                                  title: Text(
+                                    recommendedList.items[index].name,
+                                    textAlign: TextAlign.center,
                                   ),
-                                ]),
-                          ),*/
-                          /*onTap: () {
-                            _tappedCategoryCell(item.routeName);
-                          },*/
-                        ),
-                      );
-
-                      /*Center(
-                        child: Text(
-                          'Item $index',
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                      );*/
-                    }),
+                                ),
+                                child: Icon(
+                                  IconData(
+                                      recommendedList.items[index].iconData,
+                                      fontFamily: 'MaterialIcons'),
+                                  color: Colors.white,
+                                  size: 75,
+                                ),
+                              ),
+                            ),
+                          ));
+                    },
                   )
                 : Center(
                     child: Column(
