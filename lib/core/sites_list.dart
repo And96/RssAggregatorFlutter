c74c60480a83f2eaba9ebeb6ce52606dd548cd6d
aa <42185838+And96@users.noreply.github.com>
2022-12-15 20:00:00 +0100

    Dont reset category if site added more times #137

--- lib/core/sites_list.dart
+++ lib/core/sites_list.dart
@@ -6,7 +6,8 @@ import 'package:rss_aggregator_flutter/core/site.dart';
 import 'package:rss_aggregator_flutter/core/utility.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:rss_aggregator_flutter/core/site_icon.dart';
-import 'package:collection/collection.dart'; // <- required or firstWhereOrNull is not defined
+// ignore: depend_on_referenced_packages
+import 'package:collection/collection.dart';
 
 class SitesList {
   late List<Site> items = [];
@@ -196,7 +197,8 @@ class SitesList {
           (Utility().cleanUrlCompare(e.siteLink) ==
               Utility().cleanUrlCompare(site.siteLink)));
       if (siteOld != null) {
-        site.category = site.category;
+        site.siteID = siteOld.siteID;
+        site.category = siteOld.category;
         items.removeWhere((e) => (Utility().cleanUrlCompare(e.siteLink) ==
             Utility().cleanUrlCompare(site.siteLink)));
       }
