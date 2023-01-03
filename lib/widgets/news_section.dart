e0070578b4384f3510fd0f21abba25d001e28533
aa <42185838+And96@users.noreply.github.com>
2023-01-03 20:33:31 +0100

    Fix missing settings

--- lib/widgets/news_section.dart
+++ lib/widgets/news_section.dart
@@ -347,11 +347,16 @@ class _NewsSectionState extends State<NewsSection>
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         //const Divider(),
-
                         Container(
-                            height: settingsLoadImages ?
-                                MediaQuery.of(context).size.height / 100 * 50 - 100 
-                            : MediaQuery.of(context).size.height / 100 * 30 + 50,
+                            height: settings.settingsLoadImages
+                                ? MediaQuery.of(context).size.height /
+                                        100 *
+                                        50 -
+                                    100
+                                : MediaQuery.of(context).size.height /
+                                        100 *
+                                        30 +
+                                    50,
                             margin: const EdgeInsets.only(
                                 bottom: 7, top: 0, left: 0, right: 0),
                             padding: const EdgeInsets.all(0),
